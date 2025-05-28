479d9810ec2431537990cce6a984d3e34a168e8b
zintkaisoul <mendeleep262@gmail.com>
2025-05-28 21:54:38 +0700

    28052025

--- app/models.py
+++ app/models.py
@@ -13,25 +13,47 @@ class Product(models.Model):
     name = models.CharField(max_length=200,null=True)
     price = models.FloatField()
     digital = models.BooleanField(default=False,null=True,blank=False)
+    image = models.ImageField(null=True,blank=True)
 
     def __str__(self):
         return self.name
-class Oder(models.Model):
+    @property
+    def ImageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = '' 
+        return url
+class Order(models.Model):
     customer = models.ForeignKey(Customer,on_delete=models.SET_NULL,blank=True,null=True)
-    date_oder = models.DateTimeField(auto_now_add=True)
+    date_order = models.DateTimeField(auto_now_add=True)
     complete = models.BooleanField(default=False,null=True,blank=False)
     transaction_id = models.CharField(max_length=200,null=True)
 
     def __str__(self):
         return str(self.id)
-class OderItem(models.Model):
+    @property
+
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.quanlity for item in orderitems])
+        return total
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
+class OrderItem(models.Model):
     product = models.ForeignKey(Product,on_delete=models.SET_NULL,blank=True,null=True)
-    oder = models.ForeignKey(Oder,on_delete=models.SET_NULL,blank=True,null=True)
+    order = models.ForeignKey(Order,on_delete=models.SET_NULL,blank=True,null=True)
     quanlity = models.IntegerField(default=0,null=True,blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
+    @property
+    def get_total(self):
+        total = self.product.price * self.quanlity
+        return total
 class ShippingAddress(models.Model):
     customer = models.ForeignKey(Customer,on_delete=models.SET_NULL,blank=True,null=True)
-    oder = models.ForeignKey(Oder,on_delete=models.SET_NULL,blank=True,null=True)
+    order = models.ForeignKey(Order,on_delete=models.SET_NULL,blank=True,null=True)
     address = models.CharField(max_length=200,null=True)
     city = models.CharField(max_length=200,null=True)
     state = models.CharField(max_length=200,null=True)
--- app/views.py
+++ app/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
 def home(request):
-    context={}
+    products = Product.objects.all()
+    context={'products': products}
     return render(request, 'app/home.html',context)
 def cart(request):
-    context={}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer = customer,complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+    context={'items':items,'order':order}
     return render(request, 'app/cart.html',context)
 def checkout(request):
     context={}
