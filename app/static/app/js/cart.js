85d163e3474d282f8259a969821e93c63436a250
zintkaisoul <mendeleep262@gmail.com>
2025-05-31 16:46:07 +0700

    31052025 -ud

--- app/static/app/js/cart.js
+++ app/static/app/js/cart.js
@@ -31,5 +31,6 @@ function updateUserOrder(productId, action){
     })
     .then((data) =>{
         console.log('data',data)
+        location.reload()
     })
 }
--- app/views.py
+++ app/views.py
@@ -4,29 +4,44 @@ from .models import *
 import json
 
 # Create your views here.
+
+
 def home(request):
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer = customer,complete=False)
+        items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
+    else:
+        items = []
+        order = {'get_cart_items':0, 'get_cart_total':0}
+        cartItems = order['get_cart_items']
     products = Product.objects.all()
-    context={'products': products}
+    context={'products': products,'cartItems':cartItems}
     return render(request, 'app/home.html',context)
 def cart(request):
     if request.user.is_authenticated:
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer = customer,complete=False)
         items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
     else:
         items = []
         order = {'get_cart_items':0, 'get_cart_total':0}
-    context={'items':items,'order':order}
+        cartItems = order.get_cart_items
+    context={'items':items,'order':order,'cartItems':cartItems}
     return render(request, 'app/cart.html',context)
 def checkout(request):
     if request.user.is_authenticated:
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer = customer,complete=False)
         items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
     else:
         items = []
         order = {'get_cart_items':0, 'get_cart_total':0}
-    context={'items':items,'order':order}
+        cartItems = order.get_cart_items
+    context={'items':items,'order':order,'cartItems':cartItems}
     return render(request, 'app/checkout.html',context)
 def updateItem(request):
     if not request.user.is_authenticated:
